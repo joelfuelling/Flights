52343bb27d5887fc93db7fe74407f231b094ede0
Joel <JoelFuelling@gmail.com>
2023-04-13 17:23:40 -0500

    Mongo is setup

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+
+
+
+
+require('./config/database-connection')
+
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
